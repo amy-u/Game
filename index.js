7f61d1b06f91b60fc46d03082d9ed356412ac614
Amy Ung <amyung.lm@gmail.com>
2020-01-28 16:59:34 -0800

    Add background image and style to html

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function flip(){
+function myFunction(){
     function deterNum(){
-    returnMath.floor(Math.random()));
-}
+    return Math.floor((Math.random() * 2) + 1);
+    }
+    
+    var result = deterNum();
+
+    switch(result){
+    case 1:
+        text = "Heads";
+        break;
+    case 2:
+        text = "Tails"
+        break;;
+    }
+
+    document.getElementById("coin").innerHTML = text;
 }
